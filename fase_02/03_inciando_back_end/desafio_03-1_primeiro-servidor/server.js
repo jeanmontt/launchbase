b7eb9d6e0983a81685411fdf20d8bf44ed860b19
Jean Monteiro <jeanmontt@hotmail.com>
2020-12-09 19:35:30 +0000

    Finished challenge 3-1

---
+++ fase_02/03_inciando_back_end/desafio_03-1_primeiro-servidor/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server
+});
+
+server.get("/", (req, res) => {
+    return res.render("about");
+});
+
+server.get("/conteudos", (req, res) => {
+    return res.render("contents");
+});
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
+
+server.listen(5000, () => {
+    console.log("Server is running!");
+});
