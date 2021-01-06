a44ef8270712bc68fd05f948103fb05a1592e2b2
Jean Monteiro <jeanmontt@hotmail.com>
2021-01-06 21:01:58 +0000

    Adding instructor registration

---
+++ fase_03/01_controle_de_academia/#_gym_manager/instructors.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+//Create
+exports.post = function(req, res) {
+  const keys = Object.keys(req.body);
+
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send("Please, fill all fields!");
+    }
+  };
+
+  fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+    if (err) return res.send("Write file error!");
+
+    return res.redirect("/instrutores");
+  });
+
+  // return res.send(req.body);
+};
--- fase_03/01_controle_de_academia/#_gym_manager/routes.js
+++ fase_03/01_controle_de_academia/#_gym_manager/routes.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const instructors = require("./instructors");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-    return res.redirect("/instrutores");
+  return res.redirect("/instrutores");
 });
 
 routes.get("/instrutores", (req, res) => {
-    return res.render("instructors/index");
+  return res.render("instructors/index");
+});
+
+routes.post("/instrutores", instructors.post);
+
+routes.get("/instrutores/novo", (req, res) => {
+  return res.render("instructors/create");
 });
 
 routes.get("/membros", (req, res) => {
-    return res.send("members");
+  return res.send("members");
 });
 
 module.exports = routes;
