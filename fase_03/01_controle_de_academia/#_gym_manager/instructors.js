416d623a3f5c977fa4f541a7a836ae0c74314f8e
Jean Monteiro <jeanmontt@hotmail.com>
2021-01-11 15:49:07 +0000

    Finished instructor registration

--- fase_03/01_controle_de_academia/#_gym_manager/instructors.js
+++ fase_03/01_controle_de_academia/#_gym_manager/instructors.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const data = require("./data.json");
 
 //Create
 exports.post = function(req, res) {
@@ -10,7 +11,23 @@ exports.post = function(req, res) {
     }
   };
 
-  fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+  let { avatar_url, name, birth, gender, services } = req.body;
+
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.instructors.length + 1);
+
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Write file error!");
 
     return res.redirect("/instrutores");
