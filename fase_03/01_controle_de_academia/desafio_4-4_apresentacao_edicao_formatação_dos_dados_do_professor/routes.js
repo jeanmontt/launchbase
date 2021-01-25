8a362cd0d1ab17f8f63a38bddc0659afd6b8826e
Jean Monteiro <jeanmontt@hotmail.com>
2021-01-25 18:03:41 +0000

    Initial commit challenge 4-4

---
+++ fase_03/01_controle_de_academia/desafio_4-4_apresentacao_edicao_formatação_dos_dados_do_professor/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const teachers = require('./teachers');
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+  return res.redirect("/professores");
+});
+
+routes.get("/professores", (req, res) => {
+  return res.render("teachers/index");
+});
+
+routes.post("/professores", teachers.post);
+
+routes.get("/professores/novo", (req, res) => {
+  return res.render("teachers/create");
+});
+
+routes.get("/alunos", (req, res) => {
+  return res.send("students");
+});
+
+module.exports = routes;
