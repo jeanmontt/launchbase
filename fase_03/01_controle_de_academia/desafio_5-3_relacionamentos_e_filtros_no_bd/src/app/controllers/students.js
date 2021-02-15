e937c19940a5330b7d2337c96289b50f34a3e82b
Jean Monteiro <jeanmontt@hotmail.com>
2021-02-15 18:32:09 +0000

    Initial commit challenge 5-3

---
+++ fase_03/01_controle_de_academia/desafio_5-3_relacionamentos_e_filtros_no_bd/src/app/controllers/students.js
@@ -0,0 +1,78 @@
+const { grade, date } = require("../../lib/utils");
+const Student = require("../models/student");
+
+module.exports = {
+  index(req, res) {
+    Student.all((students) => {
+      students = students.map(student => {
+        const newStudent = {
+          ...student,
+          school_year: grade(student.school_year)
+        }
+
+        return newStudent;
+      });
+
+      return res.render("students/index", { students });
+    });
+  },
+
+  create(req, res) {
+    return res.render("students/create");
+  },
+
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
+    };
+
+    Student.create(req.body, (student) => {
+      return res.redirect(`/alunos/${student.id}`);
+    });
+  },
+
+  show(req, res) {
+    Student.find(req.params.id, (student) => {
+      if (!student) return res.send("Student not found!");
+
+      student.birth = date(student.birth).format;
+      student.school_year = grade(student.school_year);
+
+      return res.render("students/show", { student });
+    });
+  },
+
+  edit(req, res) {
+    Student.find(req.params.id, (student) => {
+      if (!student) return res.send("Student not found!");
+
+      student.birth = date(student.birth).iso;
+
+      return res.render("students/edit", { student });
+    });
+  },
+
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
+    };
+
+    Student.update(req.body, () => {
+      return res.redirect(`/alunos/${req.body.id}`);
+    });
+  },
+
+  delete(req, res) {
+    Student.delete(req.body.id, () => {
+      return res.redirect("/alunos");
+    });
+  }
+};
---
+++ fase_03/01_controle_de_academia/desafio_5-3_relacionamentos_e_filtros_no_bd/src/app/controllers/teachers.js
@@ -0,0 +1,80 @@
+const { age, date, graduation } = require("../../lib/utils");
+const Teacher = require("../models/teacher");
+
+module.exports = {
+  index(req, res) {
+    Teacher.all((teachers) => {
+      teachers = teachers.map(teacher => {
+        const newTeacher = {
+          ...teacher,
+          services: teacher.services.split(",")
+        }
+
+        return newTeacher;
+      });
+
+      return res.render("teachers/index", { teachers });
+    });
+  },
+
+  create(req, res) {
+    return res.render("teachers/create");
+  },
+
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
+    };
+
+    Teacher.create(req.body, (teacher) => {
+      return res.redirect(`/professores/${teacher.id}`);
+    });
+  },
+
+  show(req, res) {
+    Teacher.find(req.params.id, (teacher) => {
+      if (!teacher) return res.send("Teacher not found!");
+
+      teacher.age = age(teacher.birth);
+      teacher.graduation = graduation(teacher.graduation);
+      teacher.services = teacher.services.split(",");
+      teacher.created_at = date(teacher.created_at).format;
+
+      return res.render("teachers/show", { teacher });
+    });
+  },
+
+  edit(req, res) {
+    Teacher.find(req.params.id, (teacher) => {
+      if (!teacher) return res.send("Teacher not found!");
+
+      teacher.birth = date(teacher.birth).iso;
+
+      return res.render("teachers/edit", { teacher });
+    });
+  },
+
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
+    };
+
+    Teacher.update(req.body, () => {
+      return res.redirect(`/professores/${req.body.id}`);
+    });
+  },
+
+  delete(req, res) {
+    Teacher.delete(req.body.id, () => {
+      return res.redirect("/professores");
+    });
+  }
+};
